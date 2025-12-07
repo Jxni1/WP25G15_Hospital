23480bda2161eae574539d6f12cdf3a948a9ff1b
Jxni1 <jonramadani303@gmail.com>
2025-12-07 12:07:17 +0100

    Add project files.

---
+++ WP25G15_Hospital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WP25G15_Hospital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
